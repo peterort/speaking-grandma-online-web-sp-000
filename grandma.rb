1a53cab6d9ac5327b100685b653b39ee49a70a21
Peter Ort <peter@peterort.com>
2020-04-03 17:54:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,17 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
-  if phrase.upcase == "I LOVE YOU GRANDMA!"
+
 # HUH?! SPEAK UP, SONNY!
-    puts "I LOVE YOU TOO PUMPKIN!"
+
 # unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
-  else
-    puts "HUH?! SPEAK UP, SONNY!"
+
 # NO, NOT SINCE 1938!
 
-  end
+
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
