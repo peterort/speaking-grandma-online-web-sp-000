6d6dc4dea9e15880c85729e5d850fda97d23a1c0
Peter Ort <peter@peterort.com>
2020-04-03 18:02:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    end
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
